4de61149ebbd165346b1f301bd008660c568df54
Peter Kondacs <kondacspeter@gmail.com>
2017-11-22 11:04:18 +0100

    Renamed the in_first_half variable to in_first_try.

--- bowling.py
+++ bowling.py
@@ -1,13 +1,13 @@
 def score(game):
     result = 0
     frame = 1
-    in_first_half = True
+    in_first_try = True
     for i in range(len(game)):
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
+        # if not in_first_try:
             # frame += 1
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
@@ -19,14 +19,14 @@ def score(game):
                 else:
                     result += get_value(game[i+2])
         last = get_value(game[i])
-        if not in_first_half:
+        if not in_first_try:
             frame += 1
-        if in_first_half:
-            in_first_half = False
+        if in_first_try:
+            in_first_try = False
         else:
-            in_first_half = True
+            in_first_try = True
         if game[i] == 'X' or game[i] == 'x':
-            in_first_half = True
+            in_first_try = True
             frame += 1
     return result
 
