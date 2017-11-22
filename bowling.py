1f0d2415cbdcc8d6910c779d45914378ed3ae3a8
Peter Kondacs <kondacspeter@gmail.com>
2017-11-22 18:44:20 +0100

    Reorganized code with the definition of 2 new functions and changing the nested if conditions.

--- bowling.py
+++ bowling.py
@@ -1,28 +1,24 @@
+NUMBER_OF_FRAMES = 10
+
+
 def score(game):
-    NUMBER_OF_FRAMES = 10
     result = 0
     actual_frame = 1
     in_first_try = True
     for i in range(len(game)):
-        if game[i] == '/':
-            result += get_value(game[i]) - get_value(game[i-1])
-        else:
-            result += get_value(game[i])
-        if actual_frame < NUMBER_OF_FRAMES:
+        if actual_frame <= NUMBER_OF_FRAMES:
             if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
-                else:
-                    result += get_value(game[i+2])
-        if not in_first_try:
-            actual_frame += 1
-        in_first_try = not in_first_try
-        if game[i] == 'X' or game[i] == 'x':
-            in_first_try = True
-            actual_frame += 1
+                result += count_spare_score(game[i-1], game[i+1])
+            elif game[i] in ['X', 'x']:
+                result += count_strike_score(game[i+1], game[i+2])
+                in_first_try = True
+                actual_frame += 1
+            else:
+                result += get_value(game[i])
+            if not in_first_try:
+                actual_frame += 1
+            if game[i] not in ['X', 'x']:
+                in_first_try = not in_first_try
     return result
 
 
@@ -35,3 +31,19 @@ def get_value(char):
         return 0
     else:
         raise ValueError()
+
+
+def count_spare_score(last_roll, next_roll):
+    score = 0
+    score += get_value('/') - get_value(last_roll)
+    score += get_value(next_roll)
+    return score
+
+
+def count_strike_score(next_roll, roll_after_next):
+    score = 0
+    score += get_value('X')
+    score += get_value(next_roll) + get_value(roll_after_next)
+    if roll_after_next == '/':
+        score -= get_value(next_roll)
+    return score
