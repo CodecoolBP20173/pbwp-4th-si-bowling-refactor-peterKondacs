92ee570399aa39125b42fbc5681962ed3c779015
Peter Kondacs <kondacspeter@gmail.com>
2017-11-22 10:39:06 +0100

    Added a comprehension to the definition of get_value function instead of the long 'or' conditions if char is 1 to 9.

--- bowling.py
+++ bowling.py
@@ -31,9 +31,7 @@ def score(game):
     return result
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in [str(i) for i in range(10)]:
         return int(char)
     elif char == 'X' or char == 'x':
         return 10
